368eb8ac50dd3a5e089efb2a23cc1a4dedd1e95d
Gustavo Caraciolo <gustavo.ventura@jupiter.co.ao>
2018-09-25 14:33:34 +0100

    Adicionado projeto

---
+++ store/src/main/webapp/app/entities/entity.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { StoreProductModule } from './product/product.module';
+import { StoreProductCategoryModule } from './product-category/product-category.module';
+import { StoreCustomerModule } from './customer/customer.module';
+import { StoreProductOrderModule } from './product-order/product-order.module';
+import { StoreOrderItemModule } from './order-item/order-item.module';
+import { StoreInvoiceModule as InvoiceInvoiceModule } from './invoice/invoice/invoice.module';
+import { StoreShipmentModule as InvoiceShipmentModule } from './invoice/shipment/shipment.module';
+import { StoreNotificationModule as NotificationNotificationModule } from './notification/notification/notification.module';
+/* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
+
+@NgModule({
+    // prettier-ignore
+    imports: [
+        StoreProductModule,
+        StoreProductCategoryModule,
+        StoreCustomerModule,
+        StoreProductOrderModule,
+        StoreOrderItemModule,
+        InvoiceInvoiceModule,
+        InvoiceShipmentModule,
+        NotificationNotificationModule,
+        /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
+    ],
+    declarations: [],
+    entryComponents: [],
+    providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class StoreEntityModule {}
